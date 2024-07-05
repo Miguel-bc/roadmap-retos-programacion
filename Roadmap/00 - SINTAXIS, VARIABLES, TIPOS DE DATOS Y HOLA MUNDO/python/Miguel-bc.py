a03196e5581dea8aa9cc4207ea39814fc9dc9eb4
Miguel-bc <miguel.blaya@gmail.com>
2024-07-05 17:57:24 +0200

    primer ejercicio

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Miguel-bc.py
@@ -0,0 +1,24 @@
+
+ # Sitio web oficial de python https://www.python.org/
+ 
+"""
+
+docstring todo el texto que pongamos
+entre las 3 comillas sera comentario
+
+"""
+
+# Constantes (por convención, se usan nombres en mayúsculas)
+
+PI = 3.1416
+
+# Asignación de valores a variables
+
+edad = 18 # enteros
+coeficienteintelectual = 0.22 # flotantes
+altura = 1 + 2j # complejos
+nombre = "Miguel-bc" # cadena de texto
+guapo = False # booleanos
+empleo = None # None representa la ausencia de valores
+
+print("Hola Mundo, Python")
